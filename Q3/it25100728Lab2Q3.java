d37705d0573d820ec0b6cf0bab85ce1813215b34
IT25100728 <it25100728@my.sliit.lk>
2025-07-12 17:40:46 +0530

    Added Lab 02 Q3

---
+++ Q3/it25100728Lab2Q3.java
@@ -0,0 +1,14 @@
+public class IT25100728Lab2Q3 {
+	
+	//Main method
+	public static void main (String[] args){
+		double sideA, sideB, hypotenuse;
+		sideA = 3;
+		sideB = 4;
+		hypotenuse = Math.sqrt(Math.pow(sideA,2)+Math.pow(sideB,2));
+		
+		System.out.println("Length of hypotenuse is: "+ hypotenuse);
+		
+	}
+	
+	}
