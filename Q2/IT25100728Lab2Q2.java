be6e48198fb19896d438ce8cbc12b712cefcdfa2
IT25100728 <it25100728@my.sliit.lk>
2025-07-12 17:40:11 +0530

    Added Lab2 Q2

---
+++ Q2/IT25100728Lab2Q2.java
@@ -0,0 +1,16 @@
+public class IT25100728Lab2Q2 {
+	
+	//Main method
+	public static void main (String[] args){
+		double sLength, cCircumference, Pi, radius, sPerimeter;
+		sLength = 10;
+		sPerimeter = 4 * sLength;
+		cCircumference = sPerimeter;
+		Pi = 3.14;
+		radius = cCircumference / (2 * Pi);
+		//concat
+		System.out.println("Radius of the circuler fence is: "+ radius);
+		
+	}
+	
+	}
